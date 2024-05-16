23d37d6fff42ed304e400f55183d55a1a7998d97
Vane737 <77525733+Vane737@users.noreply.github.com>
2024-05-16 13:07:42 -0400

    add first configurations

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        // url: configService.get('DATABASE_URL'),
+        host: process.env.POSTGRES_HOST,
+        port: +process.env.POSTGRES_PORT,
+        database: process.env.POSTGRES_DATABASE,
+        username: process.env.POSTGRES_USERNAME,
+        password: process.env.POSTGRES_PASSWORD,
+        entities: ['dist/src/**/*.entity{.ts,.js}'],
+        autoLoadEntities: true, // Carga automaticamente las entidades
+        synchronize: true,  // Realiza las migraciones automaticamente
+        // ssl: true,
+        // extra: {
+        //   ssl: {
+        //     rejectUnauthorized: false,
+        //   },
+        // }
+      }),
+     }),
+  ],
+
+})
+export class AppModule {}
